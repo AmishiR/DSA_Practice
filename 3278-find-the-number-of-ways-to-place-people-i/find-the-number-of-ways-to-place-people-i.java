409c0e22b4bbb9e140d98493c2e4f07a0db76a2b
Amishi Ranjan <145252272+AmishiR@users.noreply.github.com>
2025-09-03 19:04:08 +0530

    Time: 13 ms (60.53%) | Memory: 44.9 MB (15.79%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.java
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.java
@@ -1,42 +1,45 @@
-import java.util.Arrays;
-
 class Solution {
     public int numberOfPairs(int[][] points) {
-        int n = points.length;
-        int count = 0;
+        int n=points.length;
+        int count=0;
 
-        Arrays.sort(points, (a, b) -> {
-            if (a[0] != b[0]) {
-                return Integer.compare(a[0], b[0]);
-            } else {
-                return Integer.compare(b[1], a[1]);
-            }
-        });
+        for(int i=0;i<n;i++){
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j) continue;
+            //A- UPPER WALA POINT
+            int x1=points[i][0];
+            int y1=points[i][1];
 
-                if (points[i][0] <= points[j][0] && points[i][1] >= points[j][1]) {
-                    boolean isPairValid = true;
+            //B-point
+            for(int j=0;j<n;j++){
+                boolean pointornot=false;
+                if(i==j){
+                    continue;
+                }
+                int x2=points[j][0];
+                int y2=points[j][1];
 
-                    for (int k = 0; k < n; k++) {
-                        if (k == i || k == j) continue;
+                if(x1<=x2 && y2<=y1){
 
-                        int xA = points[i][0], yA = points[i][1];
-                        int xB = points[j][0], yB = points[j][1];
-                        int xK = points[k][0], yK = points[k][1];
+                    for(int k=0;k<n;k++){
+                        if(k==i || k==j){
+                            continue;
+                        }
+                        int x3=points[k][0];
+                        int y3=points[k][1];
 
-                        if (xK >= xA && xK <= xB && yK >= yB && yK <= yA) {
-                            isPairValid = false;
+                        if(x3>=x1 && x3<=x2 && y3<=y1 && y3>=y2){
+                            pointornot=true;
                             break;
                         }
                     }
-
-                    if (isPairValid) count++;
+                    if(!pointornot){
+                        count++;
+                    }
                 }
             }
+
+
         }
-        return count;
+          return count;
     }
 }
