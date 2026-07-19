d93706d243a245ff72614d5c4be46cb766ead55d
Amy <145252272+AmishiR@users.noreply.github.com>
2026-07-20 00:47:32 +0530

    Time: 3 ms (46.59%) | Memory: 12.3 MB (56.82%) - LeetSync

---
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def smallestSubsequence(self, s):
+        freq = [0] * 26
+        seen = [False] * 26
+
+        for ch in s:
+            freq[ord(ch) - ord('a')] += 1
+
+        res = []
+
+        for ch in s:
+            idx = ord(ch) - ord('a')
+            freq[idx] -= 1
+
+            if seen[idx]:
+                continue
+
+            while res and res[-1] > ch and freq[ord(res[-1]) - ord('a')] > 0:
+                seen[ord(res[-1]) - ord('a')] = False
+                res.pop()
+
+            res.append(ch)
+            seen[idx] = True
+
+        return "".join(res)
+        
